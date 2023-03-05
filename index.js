b0069750736abaf1bbd2bd64113f48138b27f964
pixelliquid <peter@pixelliquid.com>
2023-03-05 21:47:54 +0100

    get the amount of characters using slider

--- index.js
+++ index.js
@@ -3,29 +3,47 @@ const characters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
   '/'];
 let amount = 15;
 
-// just for muscle memory we create three new arrays to hold characters, numbers and symbols
-const chars = [];
-const nums = [];
-const syms = [];
-
 // // steps for taking random characters from given array:
 // // console a character from the given array using index
 // console.log(characters[0])
 // // 1. console amount of characters in given array
 // console.log(characters.length) // 91
 // // 2. get random number within length of given array
-// console.log(Math.random() * 91)
+// console.log(Math.random() * 91) // fixed on certain amount
+// console.log(Math.random() * characters.length) // connected to array - better option
 // // 3. floor to integer
-// console.log(Math.floor(Math.random() * 91))
+// console.log(Math.floor(Math.random() * characters.length))
 // // 4. console random character using floored random index
-// console.log(characters[(Math.floor(Math.random() * 91))])
+// console.log(characters[(Math.floor(Math.random() * characters.length))])
 
+// just for muscle memory we create three new arrays to hold characters, numbers and symbols
+let chars = [];
+let nums = [];
+let syms = [];
+function createSubArrays() {
+  for (let i = 0; i < characters.length; i++) {
+    if (i < 52) {
+      chars += characters[i];
+    } else if (i > 61) {
+      syms += characters[i];
+    } else nums += characters[i];
+  };
+  console.log(chars);
+  console.log(nums);
+  console.log(syms);
+};
+
+createSubArrays();
 
 // Code for use on the HTML
 
 // the buttons
 let passwordsButton = document.getElementById('generatePasswords');
-let clearButton = document.getElementById('clearPasswords');
+
+// the input slider & display
+let amountInput = document.getElementById('amountInput');
+let amountDisplay = document.getElementById('amountDisplay');
+amountInput.value = amountDisplay.textContent = amount;
 
 // the output
 let passwordsOutputOne = document.getElementById('outputOne');
@@ -34,7 +52,7 @@ let passwordsOutputTwo = document.getElementById('outputTwo');
 
 // clean slate
 function clear() {
-  value = 0;
+  value = '';
   passwordsOutputOne.innerHTML = passwordsOutputTwo.innerHTML = '&nbsp;'
 };
 
@@ -42,7 +60,8 @@ function clear() {
 clear();
 
 // get the buttons to work for us
-// check if the button works using addEventListener
+
+// generate random passwords using addEventListener
 passwordsButton.addEventListener('click', function () {
   // console.log('it works'); // check
   // passwordsOutputOne.textContent = passwordsOutputTwo.textContent = 'it works' // check
@@ -50,16 +69,17 @@ passwordsButton.addEventListener('click', function () {
   showGeneratedPasswords();
 });
 
-// clear passwords
-clearButton.addEventListener('click', function () {
-  clear();
+// update amount of characters using range slider
+amountInput.addEventListener('input', function () {
+  amountDisplay.textContent = amountInput.value;
 });
 
 // get random characters from given array for use in outputs
 function getRandomCharacter(amount) {
-  value = 0;
+  value = ''; // clear out previous run
+  amount = amountInput.value; // get amount from input
   for (let i = 0; i < amount; i++) {
-    value += characters[(Math.floor(Math.random() * 91))];
+    value += characters[(Math.floor(Math.random() * characters.length))];
   };
   // console.log(value); // check
   return value;
@@ -70,8 +90,8 @@ function showGeneratedPasswords() {
   // first try
   // clear();
   // for (let i = 0; i < amount; i++) {
-  //   passwordsOutputOne.textContent += characters[(Math.floor(Math.random() * 91))];
-  //   passwordsOutputTwo.textContent += characters[(Math.floor(Math.random() * 91))];
+  //   passwordsOutputOne.textContent += characters[(Math.floor(Math.random() * characters.length))];
+  //   passwordsOutputTwo.textContent += characters[(Math.floor(Math.random() * characters.length))];
   // };
   // cleaner try with getRandomCharacter() function
   passwordsOutputOne.textContent = getRandomCharacter(amount);
